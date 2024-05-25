435ad8885edac57e5103acf257a8922a54fc5257
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-26 00:18:15 +0530

    Create ques10.c

---
+++ ques10.c
@@ -0,0 +1,15 @@
+// GOPAL JEE SRIVATAVA
+// 2303510100043
+#include <stdio.h>
+int main() {
+    char a,d,c;
+    printf("Enter the three characters: ");
+    scanf("%c %c %c",&a,&d,&c);
+    int p=a;
+    int q=d;
+    int r=c;
+    printf("%c----%d\n",a,p);
+    printf("%c----%d\n",d,q);
+    printf("%c----%d\n",c,r);
+    return 0;
+}
