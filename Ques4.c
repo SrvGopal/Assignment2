a5df2144a0e11b84c04cf3b77918b063f8b17aab
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-26 00:00:30 +0530

    Create Ques4.c

---
+++ Ques4.c
@@ -0,0 +1,18 @@
+// GOPAL JEE SRIVATAVA
+// ROLL NO 2303510100043
+#include <stdio.h>
+int main() {
+   int a,b,c;
+   printf("Enter the any number: ");
+   scanf("%d ",&a);
+   if(a>0){
+       printf("positive");
+   }
+   else if(a<0){
+       printf("negative");
+   }
+   else{
+       printf("zero");
+   }
+    return 0;
+}
