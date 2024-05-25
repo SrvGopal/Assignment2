41a1375ef60e0892ca4555eae8517d24ed329375
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-26 00:20:48 +0530

    Create ques11.c

---
+++ ques11.c
@@ -0,0 +1,20 @@
+// GOPAL JEE SRIVATAVA
+// 2303510100043
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    if(age>=0 && age<=12){
+        printf("Child")
+    }
+    else if(age>=13 && age<=19){
+        printf("Teenager");
+    }
+    else if(age>=20 && age<=59){
+        printf("Adult");
+    }
+    else{
+        printf("Senior");
+    }
+}
