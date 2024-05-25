9f6b30ed16e07a72ed0e073a2bfb8f0578f0e25e
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-26 00:14:35 +0530

    Create ques5.c

---
+++ ques5.c
@@ -0,0 +1,18 @@
+// GOPAL JEE SRIVATAVA
+// ROLL NO 2303510100043
+#include <stdio.h>
+int main() {
+   char c;
+   printf("Enter any character: ");
+   scanf("%c",&c);
+   if((c>='a' && c<='z') || (a>='A' && a<='Z')){
+       if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'||c=='A' || c=='E' || c=='I' || c=='O' || c=='U' ){
+           printf("Vowel");
+       }
+       else("Consonent");
+   } 
+   else{
+       printf("Given character is not alphabet");
+   }
+    return 0;
+}
