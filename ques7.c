11511e891a97115518cc29801ca98f1927526317
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-26 00:16:14 +0530

    Create ques7.c

---
+++ ques7.c
@@ -0,0 +1,10 @@
+// GOPAL JEE SRIVATAVA
+// ROLL NO 2303510100043
+#include <stdio.h>
+int main() {
+   float a;
+   printf("Enter the radius of the circle: ");
+   scanf("%f",&a);
+   printf("The circumference of the circle is %f",2*3.14*a);
+    return 0;
+}
